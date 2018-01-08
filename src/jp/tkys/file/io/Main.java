722cc8680200bcc93f1602d23997fb51b3ec2b50
Takayoshi <tkys0515@gmail.com>
2018-01-08 14:59:22 +0900

    Added file reading function.

--- src/jp/tkys/file/io/Main.java
+++ src/jp/tkys/file/io/Main.java
@@ -1,8 +1,43 @@
 package jp.tkys.file.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    try {
+            readFile("/Users/takayoshi/MyDocuments/Work/Sample.txt");
+        } catch (Exception e) {
+
+        } finally {
+
+        }
+    }
+
+    private static void readFile(String fileName) throws IOException {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(fileName);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
     }
 }
